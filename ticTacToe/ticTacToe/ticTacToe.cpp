ce481947f940b3ada39f30bc8e69b347250b29ac
robertg231 <rgorosave231@gmail.com>
2016-06-02 18:21:09 -0700

    first commit

---
+++ ticTacToe/ticTacToe/ticTacToe.cpp
@@ -0,0 +1,18 @@
+//Fresno State Beginner project
+//Create TicTacToe game
+//todo:
+//1. build board and game basics
+//2. make multiplayer game
+//3. add single player game with minimax algorithm as AI
+//4. add levels to the single player game
+
+//based off this tutorial https://www.youtube.com/watch?v=CwziaVrM_vc
+
+#include "stdafx.h"
+#include<iostream>
+
+int main()
+{
+
+	return 0;
+}
---
+++ ticTacToe/ticTacToe/ticTacToeGame.h
@@ -0,0 +1,8 @@
+#ifndef TIC_TAC_TOE_H
+#define TIC_TAC_TOE_H
+
+
+
+
+
+#endif
