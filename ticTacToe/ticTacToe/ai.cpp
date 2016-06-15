850830687cc1c9559cd4a71dc88e70788e27e01d
robertg231 <rgorosave231@gmail.com>
2016-06-15 16:49:50 -0700

    restructured ai so i can do AB pruning

--- ticTacToe/ticTacToe/ai.cpp
+++ ticTacToe/ticTacToe/ai.cpp
@@ -3,6 +3,9 @@
 #include<ctime>
 #include<cstdlib>
 
+//constant representing infinity
+const int INF = 999999;
+
 aiMove ai::getBestMove(board& currGameBoard, int currPlayerSymbol)
 {
 	//base case checking to see if someone has won
@@ -28,81 +31,89 @@ aiMove ai::getBestMove(board& currGameBoard, int currPlayerSymbol)
 		return aiMove(0);
 	}
 
-	//used to store our temp moves
-	vector<aiMove> moves;
 
-	//iterate through all the positions on the board until we find a valid spot
-	for (int y = 0; y < currGameBoard.getSize(); y++)
-	{
-		for (int x = 0; x < currGameBoard.getSize(); x++)
-		{
-			//checking for valid move
-			if (currGameBoard.getVal(x, y) == NO_VAL)
-			{
-				aiMove move;
-				move.x = x;
-				move.y = y;
+//here is where we will implement the alpha beta pruning
 
-				//temporarily set move on board****REMEMBER TO REMOVE IT!!!
-				currGameBoard.setVal(x, y, currPlayerSymbol);
+	//making a move so we can store the bestMove
+	aiMove bestMove;
+	aiMove move;
 
-				//best moves are calculated by score
 
-				//if current player is ai, we find best move for human player
-				//otherwise, we find best move for aiplayer
-				if (currPlayerSymbol == aiSymbol)
-				{
-					move.score = getBestMove(currGameBoard, playerSymbol).score;
-				}
-				else
-				{
-					move.score = getBestMove(currGameBoard, aiSymbol).score;
-				}
-
-				//storing our temp move
-				moves.push_back(move);
-
-				//removing our temp move
-				currGameBoard.setVal(x, y, NO_VAL);
-			}
-		}
-	}
-
-	//index of best move in vector
-	int bestMove = 0;
-
-	//checking all the moves made and finding the one with the highest score
-	//if we are the AI we want the highest score
-	//if we are the player(human) we want the lowest score
+	//if current player is ai, we find best move for human player
+	//otherwise, we find best move for aiplayer
 	if (currPlayerSymbol == aiSymbol)
 	{
-		//since ai wants the highest, we start off with a really small score so it gets overwritten
-		int bestScore = -100000;
-		for (int i = 0; i < moves.size(); i++)
+		//ai wants to maximize its score so we start with a super small number(aka a loss)
+		bestMove.score = -INF;
+
+		//iterate through all the positions on the board until we find a valid spot
+		for (int y = 0; y < currGameBoard.getSize(); y++)
 		{
-			if (moves[i].score > bestScore)
+			for (int x = 0; x < currGameBoard.getSize(); x++)
 			{
-				bestMove = i;
-				bestScore = moves[i].score;
+				//checking for valid move
+				if (currGameBoard.getVal(x, y) == NO_VAL)
+				{
+					//storing our move coordinates
+					move.x = x;
+					move.y = y;
+
+					//temporarily set move on board****REMEMBER TO REMOVE IT!!!
+					currGameBoard.setVal(x, y, currPlayerSymbol);
+
+					move.score = getBestMove(currGameBoard, playerSymbol).score;
+
+					if (move.score > bestMove.score)
+					{
+						bestMove = move;
+					}
+
+					//removing our temp move
+					currGameBoard.setVal(x, y, NO_VAL);
+				}
 			}
 		}
+
+		return bestMove;
 	}
 	else
 	{
-		//since player(human) wants the lowest, we start off with a really high score
-		int bestScore = 100000;
-		for (int i = 0; i < moves.size(); i++)
+		//human player wants lowest score so we give em a super high score(aka a loss)
+		bestMove.score = INF;
+
+		//:( i know.... duplicate code :(((((
+
+		//iterate through all the positions on the board until we find a valid spot
+		for (int y = 0; y < currGameBoard.getSize(); y++)
 		{
-			if (moves[i].score < bestScore)
+			for (int x = 0; x < currGameBoard.getSize(); x++)
 			{
-				bestMove = i;
-				bestScore = moves[i].score;
+				//checking for valid move
+				if (currGameBoard.getVal(x, y) == NO_VAL)
+				{
+					//storing our move coordinates
+					move.x = x;
+					move.y = y;
+
+					//temporarily set move on board****REMEMBER TO REMOVE IT!!!
+					currGameBoard.setVal(x, y, currPlayerSymbol);
+
+					move.score = getBestMove(currGameBoard, aiSymbol).score;
+
+					if (move.score < bestMove.score)
+					{
+						bestMove = move;
+					}
+
+					//removing our temp move
+					currGameBoard.setVal(x, y, NO_VAL);
+				}
 			}
 		}
+
+		return bestMove;
 	}
 
-	//returning the bestmove stored in the moves vector
-	return moves[bestMove];
 }
 
 int ai::getLevelAdjustment()
