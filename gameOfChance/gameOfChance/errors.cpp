510f7c0a1b86cd6edbba19681c0705ad3f1f7990
robertg231 <rgorosave231@gmail.com>
2016-06-19 22:01:20 -0700

    starting on game of chance

---
+++ gameOfChance/gameOfChance/errors.cpp
@@ -0,0 +1,27 @@
+#include "stdafx.h"
+#include <string>
+
+//a function to display an error message and then exit
+void fatal(char* message)
+{
+	char errorMessage[100];
+
+	strcpy(errorMessage, "[!!] Fatal Error ");
+	strncat(errorMessage, message, 83);
+	perror(errorMessage);
+	exit(-1);
+}
+
+//an error checked malloc() wrapper function
+void* ecMalloc(unsigned int size)
+{
+	void* ptr;
+	ptr = malloc(size);
+	
+	if (ptr == NULL)
+	{
+		fatal("in ecMalloc() on memory allocation");
+	}
+
+	return ptr;
+}
---
+++ gameOfChance/gameOfChance/gameOfChance.cpp
@@ -0,0 +1,13 @@
+//gameOfChance
+//taken from Hacking Art Of Exploitation pg 102-110
+//Rewritting their code so i understand it better
+//and actually see it run
+
+#include <cstdio>
+#include <string>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <ctime>
+#include <cstdlib>
+#include "errors.cpp"
+
