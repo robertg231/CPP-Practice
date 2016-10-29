d65c12c55e6721990af9d2f585a48d14d6f4b267
Roberto <rgorosave231@gmail.com>
2016-10-29 14:18:21 -0700

    working on youtubeMP3 downloader

---
+++ youtubeMP3/youtubeMP3Main.cpp
@@ -0,0 +1,42 @@
+//program meant to make it easy to download mp3s from youtube using 
+//youtube-dl from https://github.com/rg3/youtube-dl
+//REQUIRES youtube-dl to be installed.
+
+#include<iostream>
+#include<string>
+#include<cstdlib>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	if(argc < 2)
+	{
+		if(argv[0])
+		{
+			cout << "just put URL(of a song or playlist) after the program name" << endl;
+			cout << "EXAMPLE ->  youtubeMP3 <someURL>" << endl;
+		}
+		else
+		{
+			cout << cout << "Usage: <program name> <URL>" << endl;
+		}
+
+		exit(1);
+	}
+
+	//parameters used to download mp3 file from youtube video
+	string parameters = "youtube-dl -x --audio-format mp3 --audio-quality 0 --embed-thumbnail --add-metadata ";
+	parameters += argv[1];
+
+	//testing output
+	//cout << parameters << endl;
+	
+	//does system call to youtube-dl with url
+	system(parameters.c_str());	
+	
+	//does system call to move file to Music folder
+	system("mv ./*.mp3 ~/Music/");
+
+	return 0;
+}
